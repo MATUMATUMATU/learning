4cd3efb832fd2ff263d230cdbadc420963da5b02
EinarP <einarpo@gmail.com>
2023-05-30 21:16:03 +0300

    test added

--- test_conv_1001_movies.py
+++ test_conv_1001_movies.py
@@ -9,7 +9,8 @@ def compare_files(file1_path, file2_path):
 
             for line_num, (line1, line2) in enumerate(zip(lines1, lines2), start=1):
                 if line1 == line2:
-                    print(f"Line {line_num}: Similar")
+                    pass
+#                    print(f"Line {line_num}: Similar")
                 else:
                     print(f"Line {line_num}: Not similar")
 
@@ -17,7 +18,7 @@ def compare_files(file1_path, file2_path):
         return "One or both files do not exist."
 
 # Provide the file paths of the files to compare
-file1_path = '1001_movies_ok.csv'
-file2_path = '1001_movies.csv'
+file1_path = 'imdb/data/1001_movies.csv'
+file2_path = 'imdb/data/1001_movies_ok.csv'
 
 compare_files(file1_path, file2_path)
